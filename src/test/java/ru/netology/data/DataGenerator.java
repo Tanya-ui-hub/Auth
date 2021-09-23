10cb663605f6593bd3af17e4d374046b4141b325
Tanya-ui-hub <tatyanapenzina92@gmail.com>
2021-09-23 11:00:38 +0300

    Исправление ошибок

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -5,6 +5,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import lombok.*;
 
 import java.util.Locale;
 
